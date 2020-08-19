046382a2b8b3cdc3e9db50af0c367e18b7310e00
Guillaume Lodi <guillaume.lodi@vestiairecollective.com>
2020-08-19 12:52:07 +0200

    1. Display some information on list

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,81 @@
 <script>
-import SecondPart from './SecondPart.vue';
+import SecondPart from "./SecondPart.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: { SecondPart },
-  data () {
+  data() {
     return {
+      selectedPersonInfo: undefined,
       people: [
         {
-          firstName: 'Hello',
-          lastName: 'Kitty',
-          id: 'hellok'
+          firstName: "Hello",
+          lastName: "Kitty",
+          id: "hellok",
         },
         {
-          firstName: 'Peppa',
-          lastName: 'Pig',
-          id: 'peppap'
+          firstName: "Peppa",
+          lastName: "Pig",
+          id: "peppap",
         },
         {
-          firstName: 'SpongeBob',
-          lastName: 'SquarePants',
-          id: 'spong'
+          firstName: "SpongeBob",
+          lastName: "SquarePants",
+          id: "spong",
         },
       ],
       peopleInfo: [
         {
           address: {
-            street: 'Pink St.',
-            country: 'Japan'
+            street: "Pink St.",
+            country: "Japan",
           },
-          'Secret Info': {
-            dont: 'showme'
+          "Secret Info": {
+            dont: "showme",
           },
-          personId: 'hellok',
-          quote: 'Meow',
-          status: 'Fictional'
+          personId: "hellok",
+          quote: "Meow",
+          status: "Fictional",
         },
         {
           address: {
-            street: 'Pineapple under the sea',
-            country: 'The sea'
+            street: "Pineapple under the sea",
+            country: "The sea",
           },
-          'Want to know more?': {
-            dont: 'showme'
+          "Want to know more?": {
+            dont: "showme",
           },
-          personId: 'spong',
-          quote: 'Where is Patrick?',
-          status: 'Fictional'
+          personId: "spong",
+          quote: "Where is Patrick?",
+          status: "Fictional",
         },
         {
           address: {
-            street: 'Pig St.',
-            country: 'Mud Puddles Country'
+            street: "Pig St.",
+            country: "Mud Puddles Country",
           },
-          'Secret Info': {
-            dont: 'showme'
+          "Secret Info": {
+            dont: "showme",
           },
-          'Want to know more?': {
-            dont: 'showme'
+          "Want to know more?": {
+            dont: "showme",
           },
-          personId: 'peppap',
-          quote: 'My most favorite thing in the whole world is jumping up and down in muddy puddles!',
-          status: 'Fictional'
+          personId: "peppap",
+          quote:
+            "My most favorite thing in the whole world is jumping up and down in muddy puddles!",
+          status: "Fictional",
         },
       ],
-    }
-  }
-}
+    };
+  },
+  methods: {
+    select: function (person) {
+      this.selectedPersonInfo = this.peopleInfo.find(
+        (info) => info.personId === person.id
+      );
+    },
+  },
+};
 </script>
 
 <template>
@@ -75,20 +84,29 @@ export default {
     <h2>Test by Guillaume Lodi</h2>
 
     <ul>
-      <li v-for="person in people" :key="person.id">{{ person.firstName }} {{ person.lastName }}</li>
+      <li
+        v-for="person in people"
+        @click="select(person)"
+        :key="person.id"
+        v-bind:class="{ active: selectedPersonInfo && person.id === selectedPersonInfo.personId }"
+      >{{ person.firstName }} {{ person.lastName }}</li>
     </ul>
 
-    <!-- Do the first part here -->
+    <div v-if="selectedPersonInfo" class="info">
+      <p v-for="(value, prop) in selectedPersonInfo" :key="prop">
+        <b>{{ prop }}:</b>
+        {{ typeof value === 'object' ? '[+]' : value }}
+      </p>
+    </div>
 
     <!-- Uncomment to start the second part
-    <SecondPart /> -->
+    <SecondPart />-->
   </div>
 </template>
 
 <style lang="css">
-
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -96,7 +114,8 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -109,14 +128,26 @@ ul {
 
 li {
   margin: 0 10px;
-  margin: .25rem auto;
+  margin: 0.25rem auto;
   background: #efefef;
   border-radius: 3px;
   padding: 1rem;
   min-width: 350px;
+  cursor: pointer;
+}
+
+li.active {
+  outline: 2px dotted #3498db;
 }
 
 a {
   color: #42b983;
+}
+
+.info {
+  text-align: left;
+  border: 1px solid #efefef;
+  border-radius: 3px;
+  padding: 1rem;
 }
 </style>
